60d33b97b219179eb064176358b8645e3879c52f
Rust Stnard <rust.stnard+git@gmail.com>
2009-04-17 11:34:46 +0900

    net/http 的なURLにも反応していたのを修正
    Signed-off-by: Rust Stnard <rust.stnard+git@gmail.com>

--- tinyurl.rb
+++ tinyurl.rb
@@ -5,8 +5,12 @@ Termtter::Client.register_hook(
   :points => [:modify_arg_for_update],
   :exec_proc => lambda {|cmd, arg|
     arg = arg.gsub(URI.regexp) do |url|
-      Termtter::API.connection.start('tinyurl.com', 80) do |http|
-        http.get('/api-create.php?url=' + URI.escape(url)).body
+      if url =~ %r!^https?:!
+        Termtter::API.connection.start('tinyurl.com', 80) do |http|
+          http.get('/api-create.php?url=' + URI.escape(url)).body
+        end
+      else
+        url
       end
     end
   }
